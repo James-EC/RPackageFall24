6ffc18c8a92e72f8d956554ab61bb926632616a7
James-EC <jamescunningham0321@gmail.com>
2024-10-29 00:09:43 -0500

    Implemented new function myboot2

---
+++ R/myboot2.R
@@ -0,0 +1,44 @@
+#' Title
+#'
+#' @param iter number of times code will run, more iterations leads to better results
+#' @param x a vector of values
+#' @param fun function that can look for desired statistic
+#' @param alpha probability that the statistic does not lie within calculated range
+#' @param ... parameters for the histogram output
+#'
+#' @return a histogram and a list of statistics that might be of interest
+#' @importFrom graphics segments
+#' @importFrom graphics text
+#' @importFrom stats quantile
+#' @export
+#'
+#' @examples
+#' myboot2(iter=1000, x = (1:10), col="#F34567")
+myboot2<-function(iter=10000,x,fun="mean",alpha=0.05,...){
+  #Notice where the ... is repeated in the code
+  n=length(x) #sample size
+  #Now sample with replacement
+  y=sample(x,n*iter,replace=TRUE) #A
+  # Make a matrix with all the resampled values
+  rs.mat=matrix(y,nrow=n,ncol=iter,byrow=TRUE)
+  xstat=apply(rs.mat,2,fun)
+  # xstat is a vector and will have iter values in it
+  ci=quantile(xstat,c(alpha/2,1-alpha/2)) #B
+  # Nice way to form a confidence interval
+  # A histogram follows
+  # The object para will contain the parameters used to make the histogram
+  para=hist(xstat,freq=FALSE,las=1,main="Histogram of Bootstrap sample
+  statistics",...)
+  #mat will be a matrix that contains the data, this is done so that I can use apply()
+  mat=matrix(x,nrow=length(x),ncol=1,byrow=TRUE)
+  #pte is the point estimate
+  #This uses whatever fun is
+  pte=apply(mat,2,fun)
+  abline(v=pte,lwd=3,col="Black")# Vertical line
+  segments(ci[1],0,ci[2],0,lwd=4) #Make the segment for the ci
+  text(ci[1],0,paste("(",round(ci[1],2),sep=""),col="Red",cex=3)
+  text(ci[2],0,paste(round(ci[2],2),")",sep=""),col="Red",cex=3)
+  # plot the point estimate 1/2 way up the density
+  text(pte,max(para$density)/2,round(pte,2),cex=3)
+  return(list(ci=ci,fun=fun,x=x,xstat=xstat))# Some output to use if necessary
+}
